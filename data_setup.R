25b1bd6488a0d4818b1a4f00db48ce0fa511a037
Yodae Lim <yodae.lim@gmail.com>
2018-05-28 12:28:04 -0700

    time fix and added month to table

--- data_setup.R
+++ data_setup.R
@@ -6,18 +6,18 @@ library(maps)
 # set up data
 data <- read.csv("data/cleaned_seattle_police_data.csv", stringsAsFactors = FALSE)
 
-data <- data %>% 
+data <- data %>%
   mutate(
-    date = substring(data$Event.Clearance.Date, 1, 10),
-    year = substring(data$Event.Clearance.Date, 7, 10),
-    time = substring(data$Event.Clearance.Date, 12, 22)
-  )
+         date = substring(data$Event.Clearance.Date, 1, 10),
+         year = substring(data$Event.Clearance.Date, 7, 10),
+         time = substring(data$Event.Clearance.Date, 12, 22),
+         month = substring(data$Event.Clearance.Date, 1, 2)
+         )
 
 # set up time as numerical value (hour.minute)
-if (substring(data$time, 10, 11) == "PM") {
-  hour <- as.numeric(substring(data$time, 1, 2)) + 12
-} else {
-  hour <- substring(data$time, 1, 2)
-}
+hour <- substring(data$time, 1, 2)
+hour[substring(data$time, 10, 11) == "PM"] <- hour[substring(data$time, 10, 11) == "PM"] + 12
+
 data$time <- as.numeric(paste0(hour, ".", substring(data$time, 4, 5)))
+
 
