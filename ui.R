54b2a35a05a566229d17a6e8d4469efec75b9d8b
Jae <rimjae14@uw.edu>
2018-05-29 15:30:41 -0700

    initial ui setup complete

--- ui.R
+++ ui.R
@@ -1,7 +1,23 @@
-source("data_setup.R")
+library(shiny)
 
-ui <- fluidPage(
-  
+my_ui <- fluidPage(
+  titlePanel("title of project"),
+  p(em("our names")),
+  navbarPage("Introduction of Project",
+      p(em("introduction paragraph")),
+      tabPanel("Part 1"),
+      tabPanel("Part 2",
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput("q2hour", "Hour", min = 1, max = 24, value = 1)
+          ),
+        mainPanel(
+          #plot
+        )
+        )
+      ),
+      tabPanel("Part 3"),
+      tabPanel("Part 4"))
 )
 
-shinyUI(ui)
+shinyUI(my_ui)
