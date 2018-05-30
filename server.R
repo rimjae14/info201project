333a346742e9fa975b31cfb02e056763fd5e1fa4
Jae <rimjae14@uw.edu>
2018-05-30 14:20:16 -0700

    all parts merged

--- server.R
+++ server.R
@@ -1,12 +1,81 @@
 library(shiny)
+library(plotly)
+library(DT)
+library(leaflet)
+
+# Data source
+data <- read.csv(file = "data/last5_seattle_police_data.csv", stringsAsFactors = FALSE)
+
+#Question 1 setup
+major_crimes <- c(
+  "ASSAULTS",
+  "BURGLARY",
+  "HOMICIDE",
+  "NARCOTICS COMPLAINTS",
+  "PROSTITUTION",
+  "PROPERTY DAMAGE",
+  "ROBBERY"
+)
+unique_districts <- unique(data$District.Sector)
+major_crime_data <- data %>% filter(Event.Clearance.Group %in% major_crimes)
+min_year <- min(as.numeric(data$year), na.rm = TRUE)
+max_year <- max(as.numeric(data$year), na.rm = TRUE)
+
+#Question 2 set up
 source("question2_data.R")
 
+
 my_server <- function(input, output) {
-  ##Question 1
+
+  #################### Question 1 ####################
+  ####################################################
+  filtered_major_crime_data <- reactive({
+    if (length(input$year) == 1) {
+      filtered <- major_crime_data %>%
+        filter(year == input$year[1])
+    }else{
+      filtered <- major_crime_data %>%
+        filter(year >= input$year[1] &
+                 year <= input$year[2])
+    }
+    if (!is.null(input$district)) {
+      filtered <- filtered %>% filter(District.Sector %in% input$district)
+    }
+    filtered
+  })
+  
+  hourly_data <- reactive({
+    filtered_major_crime_data() %>%
+      mutate(hr = floor(time)) %>%
+      group_by(hr, Event.Clearance.Group) %>%
+      summarize(
+        count = n()
+      )
+  })
+  
+  output$controls <- renderUI({
+    tagList(
+      sliderInput("year", "Year Range", value = c(min_year + 1, max_year - 1), min = min_year, max = max_year),
+      selectizeInput("district", label = "Select District", choices = unique_districts, options = list(maxItems = 5))
+    )
+  })
+  
+  output$monthly <- renderPlot(
+    ggplot(data = filtered_major_crime_data(), aes(month)) +
+      geom_bar(aes(fill = District.Sector)) +
+      labs(title = "Crime frequency vs Month", x = "Month of Year", y = "Crime Count")
+  )
+  
+  output$hourly <- renderPlotly({
+    ggplot(hourly_data(),aes(hr, count, color = Event.Clearance.Group)) +
+      geom_line(size = 2) +
+      geom_point() +
+      labs(title = "Crime frequency vs Hour", x = "Hours after 12AM", y = "Crime Count")
+  })
   
   
-  
-  ##Question 2
+  #################### Question 2 ####################
+  ####################################################
   question2 <- reactive({
     numhour <- as.numeric(input$q2hour)
     filtered_data <- sector_data %>%
@@ -15,21 +84,169 @@ my_server <- function(input, output) {
     return(filtered_data)
   })
   
-  output$hours <- renderTable(
-    return(question2())
-    
-  )
   output$question2graph <- renderPlot({
     p <- ggplot(data = question2()) +
-      geom_bar(mapping = aes(x = sea_precinct))
+      geom_bar(mapping = aes(x = sea_precinct, color = sea_precinct)) +
+      labs(title = "Auto Related Crimes in Seattle by Hour",
+           x = "Precincts",
+           y = "Count")
     
     return(p)
   })
   
-  ##Question 3
+  #################### Question 3 ####################
+  ####################################################
+  data_filter <- reactive({
+    data_type <- data %>%
+      filter(year == input$year) %>%
+      filter(Event.Clearance.Group == input$crime_type) %>%
+      select(District.Sector, Longitude, Latitude)
+  })
+  
+  freq_filter <- reactive({
+    freq_table <- as.data.frame(table(data_filter()[ , "District.Sector"]))
+  })
+  
+  district_filter <- reactive({
+    district_join <- inner_join(data_filter(), freq_filter(), by = c("District.Sector" = "Var1"))
+  })
+  
+  freq_district <- reactive({
+    breakpoints = c(0, 50, 100, 150, 200, 250, 300)
+    freq_district <- district_filter() %>%
+      mutate(Frequency = cut(district_filter()$Freq, breaks = breakpoints)) %>%
+      filter(Freq != 'NA')
+  })
   
   
-  ##Question 4
+  output$map <- renderPlot({
+    plot <- ggplot(freq_district()) +
+      geom_point(mapping = aes(x = Longitude, y = Latitude, color = Frequency)) +
+      scale_color_brewer(palette = "Blues") +
+      
+      labs(title = paste(input$crime_type, "Frequency in Different Areas for the year ", input$year))
+    return(plot)
+  })
+  
+  output$info <- renderPrint({
+    clicked <- nearPoints(freq_district(), input$plot_click)
+    paste("District:", clicked$District.Sector[1], "Frequency:", clicked$Freq[1])
+  })
+  
+  
+  output$plot_description <- renderText({
+    map_description <- paste0("This map shows the frequency of certain crimes compared in
+                              different districts. As the gradient darkens, the frequency
+                              of crimes are increased.")
+    return(map_description)
+  })
+  
+  output$interactive_map <- renderLeaflet({
+    map_interactive <- leaflet() %>%
+      addTiles() %>%
+      addMarkers(lng = data_filter()$Longitude, lat = data_filter()$Latitude)
+    return(map_interactive) 
+  })
+  
+  #################### Question 4 ####################
+  ####################################################
+  
+  filtered_acc_plot <- reactive({
+    accident_graph_data <- accident_data %>% 
+      filter(input$time[1] < time, input$time[2] > time) %>% 
+      filter(input$district ==  District.Sector) %>% 
+      mutate(time.round = floor(time))
+  })
+  
+  filtered_acc_plot_one <- reactive({
+    count(filtered_acc_plot(), vars = time.round)
+  })
+  
+  q4data <- reactiveValues()
+  
+  # click interaction: graph one
+  q4data$selected_time_one <- ""
+  q4data$selected_freq <- ""
+  q4data$selected_time <- ""
+  
+  # click interaction: graph two
+  q4data$selected_location <- ""
+  q4data$selected_time_two <- NULL
+  q4data$selected_dist <- ""
+  
+  
+  # GRAPH ONE
+  output$acc_graph_one <- renderPlot({
+    if (!is.null(input$district)) {  
+      ggplot(q4data = filtered_acc_plot_one()) +
+        geom_point(mapping = aes(
+          x = vars, y = n,
+          color = (vars == q4data$selected_time)
+        ), size = 3) +
+        guides(color = FALSE) +
+        labs(
+          y = "Count of traffic accidents",
+          x = "Time (hour)"
+        ) 
+    }
+  })
+  
+  # click interaction: graph one
+  output$select_time_one <- renderText({
+    q4data$selected_time_one
+  })
+  
+  output$freq <- renderText({
+    q4data$selected_freq
+  })
+  
+  observeEvent(input$plot_click_time, {
+    selected <- nearPoints(filtered_acc_plot_one(), input$plot_click_time)
+    q4data$selected_time_one <- paste0(selected$vars, ":00")
+    q4data$selected_time <- selected$vars
+    q4data$selected_freq <- selected$n
+  })
+  
+  
+  # GRAPH TWO
+  output$acc_graph_two <- renderPlot({
+    if (!is.null(input$district)) {
+      ggplot(q4data = filtered_acc_plot()) +
+        geom_point(mapping = aes(
+          x = Longitude, y = Latitude,
+          color = (Hundred.Block.Location == q4data$selected_location)
+        ), size = 1.5) +
+        guides(color = FALSE) +
+        coord_equal()
+    }
+  })
+  
+  #click interaction: graph two
+  output$location <- renderText({
+    paste(unique(tolower(q4data$selected_location)), collapse = ", ")
+  })
+  
+  output$num_selected <- renderText({
+    if (!is.null(q4data$selected_time_two)) {
+      length(q4data$selected_time_two)
+    }
+  })
+  
+  output$select_time_two <- renderText({
+    paste(sort(q4data$selected_time_two), collapse = ", ")
+  })
+  
+  output$select_dist <- renderText({
+    q4data$selected_dist[1]
+  })
+  
+  observeEvent(input$plot_click_map, {
+    selected <- nearPoints(filtered_acc_plot(), input$plot_click_map)
+    q4data$selected_location <- selected$Hundred.Block.Location
+    q4data$selected_time_two <- selected$time
+    q4data$selected_dist <- selected$District.Sector[1]
+  })
+  
 }
 
 shinyServer(my_server)
--- ui.R
+++ ui.R
@@ -1,27 +1,144 @@
 library(shiny)
+library(plotly)
+library(DT)
+library(leaflet)
+
+data <- read.csv(file = "data/last5_seattle_police_data.csv", stringsAsFactors = FALSE)
 
 my_ui <- fluidPage(
   titlePanel("title of project"),
   p(em("our names")),
   navbarPage("Introduction of Project",
       p(em("introduction paragraph")),
-      tabPanel("Part 1"),
+      tabPanel("Part 1",
+        sidebarLayout(
+          sidebarPanel(
+            # district selection widget and year slider
+            h3("Controls"),
+            uiOutput("controls"),
+            p(strong("Note:"),"If the district input is empty, both charts will be plotting data from all seattle districts"),
+            br(),
+            h4("Suggested District Combinations"),
+            tags$ul(
+              tags$li("South West Precinct: W, F"),
+              tags$li("East Precinct: C, G, E"),
+              tags$li("South Precinct: O, R, S"),
+              tags$li("West Precinct: D, K, M, Q"),
+              tags$li("North Precinct: B, L, J, N, U"),
+              tags$li("Udistrict: U")
+            )
+          ),
+          mainPanel(
+            h1("Crime Over Time"),
+            hr(),
+            h3("How does crime change over the course of a day and over the course of
+               a year in each region?"),
+            plotOutput("monthly"),# monthly crime
+            plotlyOutput("hourly"), # hourly crime data
+            h3("Monthly Analysis"),
+            p("Through experimenting with the districts and years in the monthly 
+              chart, there has been a couple of patterns that have stood out."),
+            p("If we look at Seattle with no district filter, we see a simple crime
+              pattern. Crime maxes out in June and July and hits a minimum in 
+              December and august. This must be a result of the gradual temperature
+              changes during a year. With higher temperatures, there are more open
+              windows to increase air flow and there are more outdoor social 
+              activities that are natural contributors to a variety of crimes."),
+            p("If we switch to District U, University District, we see another
+              pattern. When we select district U with any combination of years,
+              we find that month #6, June, is a clear outlier in crime count with
+              usually 1.25x to 2x the crime compared to any of the other years. The
+              end of spring quarter accompanied with drastic increase in temperature
+              is the reason for this. The end of spring quarter is when most students
+              move out of their apartments, houses and dorms, and new summer interns
+              and summer school students move in. Doors are often left open to make
+              moving easier.  This opening along with careless new residents and the 
+              general summer crime trend mentioned previously must increase crime such
+              as burglary and robbery in June."),
+            hr(),
+            h3("Hourly Analysis"),
+            p("Through experimenting with districts and years in the hourly chart we 
+              see another general pattern."),
+            p("If we observe Seattle with no filter, we see two huge spikes in all 
+              crimes excluding homicide and narcotics at 11AM, and 7PM. I think the 
+              cause for this is a combination of dinner and lunch peak hour 
+              contributing to increased human interaction. The more people are out,
+              the more interactions there are that would lead to conflicts like 
+              assault, and the more open housing there is, the more likely it is 
+              that burglaries and robberies will occur."),
+            hr()
+          )
+        )
+      ),
       tabPanel("Part 2",
         sidebarLayout(
           sidebarPanel(
-            sliderInput("q2hour", "Hour", min = 1, max = 24, value = 1)
+            h3("Control - Hour"),
+            sliderInput("q2hour", "Hour - Military time", min = 1, max = 24, value = 1)
           ),
           mainPanel(
             #plot
-            plotOutput("question2graph"),
-            tableOutput('hours')
-          
+            plotOutput("question2graph")
+            # insert image of precinct and district
           )
-        
-          )
+        )
       ),
-      tabPanel("Part 3"),
-      tabPanel("Part 4"))
+      tabPanel("Part 3",
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput('year',
+                        "Choose a year:",
+                        min = 2014,
+                        max = 2018,
+                        value = 2016,
+                        sep = ""),
+            selectInput('crime_type',
+                        "Select a crime",
+                        choices = list('Crimes' = c("ASSAULTS", "SHOPLIFTING", "BURGLARY",
+                                                    "PROPERTY DAMAGE", "PROSTITUTION", "ROBBERY",
+                                                    "AUTO THEFTS", "THREATS, HARASSMENT", "TRESPASS"))
+            )
+          ),
+          mainPanel(
+            tabsetPanel(type = 'tabs',
+                        tabPanel('Plot',
+                                 h1("Crime Frequency Map"),
+                                 plotOutput('map', click = "plot_click"),
+                                 verbatimTextOutput("info"),
+                                 p(strong("District:"), textOutput("plot_description")),
+                                 br(),
+                                 leafletOutput('interactive_map')
+                        )
+            )
+          )
+        )
+      ),
+      tabPanel("Part 4",
+        sidebarLayout(
+          sidebarPanel(
+              sliderInput(
+                  'time', label = "Time (hour.minute)",
+                  min = time_range[1], max = time_range[2], value = time_range),
+              checkboxGroupInput(
+                  'district', label = "District",
+                  choices = districts, selected = "S")
+          ),
+          mainPanel(
+              h3("Number of Traffic Accidents per Hour of Day"),
+              plotOutput('acc_graph_one', click = "plot_click_time"),
+              p(strong("Time: "), textOutput('select_time_one', inline = TRUE)),
+              p(strong("Number of Accidents: "), textOutput('freq', inline = TRUE)),
+                   
+              h3("Seattle District Mapping of Accidents"),
+              plotOutput('acc_graph_two', click = "plot_click_map"),
+              p(strong("District: "), textOutput('select_dist', inline = TRUE)),
+              p(strong("Number of Accidents: "), textOutput('num_selected', inline = TRUE)),
+              p(strong("Hundred Block Location: "), textOutput('location', inline = TRUE)),
+              p(strong("Time(s): "), textOutput('select_time_two', inline = TRUE))
+          )
+        )
+      )
+    )
 )
 
 shinyUI(my_ui)
