7c79ab47f0f8bf64cc02b6b2a98ab7e62abe771a
lenad90 <lenad98@uw.edu>
2018-05-30 23:59:30 -0700

    +

--- server.R
+++ server.R
@@ -80,12 +80,13 @@ my_server <- function(input, output) {
   
   #################### Question 3 ####################
   ####################################################
-  
+  #Takes the user's inputs and filters the appropriate data for it
   data_filter <- reactive({
     data_type <- data %>%
       filter(year == input$year) %>%
       filter(Event.Clearance.Group == input$crime_type) %>%
       select(District.Sector, Longitude, Latitude)
+    return(data_type)
   })
   
   freq_filter <- reactive({
@@ -104,15 +105,16 @@ my_server <- function(input, output) {
     freq_district <- district_filter() %>%
       mutate(Frequency = cut(district_filter()$Freq, breaks = breakpoints)) %>%
       filter(Frequency != 'NA')
+    return(freq_district)
   })
   
-  
+  #creates the scatterplot map with the given inputs
   output$map <- renderPlot({
     plot <- ggplot(freq_district()) +
       geom_point(mapping = aes(x = Longitude, y = Latitude, color = Frequency)) +
       scale_color_brewer(palette = "Blues") +
       
-      labs(title = paste(input$crime_type, "Frequency in Different Areas for the year ", input$year))
+      labs(title = paste(input$crime_type, " Crime Frequency in Different Districts of Seattle For The Year ", input$year))
     return(plot)
   })
   
@@ -128,7 +130,6 @@ my_server <- function(input, output) {
     paste(clicked()$Freq[1])
   })
   
-  
   output$plot_description <- renderText({
     map_description <- paste0("This map shows the frequency of certain crimes compared in
                               different districts. As the gradient darkens, the frequency
@@ -139,8 +140,8 @@ my_server <- function(input, output) {
     return(map_description)
   })
   
+  #creates an interactive map with a geographic map of the city
   output$interactive_map <- renderLeaflet({
-    districts <- colorFactor(c("navy", "red"), domain = c("ship", "pirate"))
     map_interactive <- leaflet() %>%
       addTiles() %>%
       addCircleMarkers(fillOpacity = 0.5,
@@ -150,14 +151,15 @@ my_server <- function(input, output) {
   
   output$plot_interactive <- renderText({
     interactive_description <- paste0("This map also shows the frequency of certain crimes compared in
-                              different districts. However, this map provides a different visualization
-                              by providing a map of the city. Each point on the map represents a data point,
-                              and the density of points in certain locations of the map provides users with
-                              a visual of the crime counts. Users are able to click on the points 
-                              to view the different districts.")
+                                      different districts. However, this map provides a different visualization
+                                      by providing a map of the city. Each point on the map represents a data point,
+                                      and the density of points in certain locations of the map provides users with
+                                      a visual of the crime counts. Users are able to click on the points 
+                                      to view the different districts.")
     return(interactive_description)
   })
   
+  #selects most dangerous districts with the user inputs
   output$most_dangerous <- renderTable({
     most_dangerous <- freq_filter() %>%
       arrange(desc(Freq)) %>%
@@ -165,11 +167,37 @@ my_server <- function(input, output) {
     head(most_dangerous, 5)
   })
   
+  #selects most safest districts with the user inputs
   output$most_safe <- renderTable({
     most_safe <- freq_filter() %>%
       arrange(Freq) %>%
       select(District, Freq)
     head(most_safe, 5)
+  })
+  
+  #Data analysis of selected data
+  output$selected_analysis <- renderText({
+    paste0("The total amount of ", input$crime_type, " crimes that have occured in ", input$year, " is ",
+           sum(freq_filter()[,'Freq']), ". The mean frequency of crimes of all the districts is ", round(mean(freq_filter()[,'Freq'])),
+           ", with the median of ", round(median(freq_filter()[,'Freq'])), " for the given selections. The outliers are also
+           displayed in the two tables above, indicating the most dangerous and safest districts in Seattle 
+           for a given crime and year. With this data, those who are planning to move to seattle can compare the districts 
+           with their frequency count to the average frequency of that certain year.")
+  })
+  
+  #overall analysis of the data
+  output$overall_analysis <- renderText({
+    paste0("Crime rate in Seattle over the ", nrow(crime_rate_data), " years is ", crime_rate, " per year. 
+           We can predict that the crime rate will continue to increase at this rate over 
+           the upcoming years. However, crimes that happened in the year 2014 are insufficient compared to the later years.
+           This is because the data does not provide many reports during this year, thus, may affect the accuracy of the
+           crime rates stated above. The district with the most crimes overall is ", max_district[1], " with a total of ", max_crime, 
+           " crimes. The district with the least crimes overall is ", min_district[1], " with a total of ", min_crime, " crimes.
+           This may indicate the type of area that makes up the district. For example, districts with lower crime rates may be
+           of suburban areas or gated communities, and districts with higher crime rates may compose of more public and open areas. 
+           By comparing the total crimes that occurred in these districts, the user can infer whether or not 
+           the change in data will be drastic or slow given the overall crime rate. As a result, future settlers in Seattle can use this data
+           to predict changes in crimes over the next years, and the change in frequency of crimes corresponding to certain districts.")
   })
   
   #################### Question 4 ####################
--- ui.R
+++ ui.R
@@ -91,47 +91,42 @@ my_ui <- fluidPage(
         )
       )
     ),
-    tabPanel(
-      "Part 3",
-      titlePanel("Crime Frequency"),
-      sidebarLayout(
-        sidebarPanel(
-          sliderInput("year",
-            "Choose a year:",
-            min = 2014,
-            max = 2018,
-            value = 2016,
-            sep = ""
-          ),
-          selectInput("crime_type",
-            "Select a crime",
-            choices = list("Crimes" = c(
-              "ASSAULTS", "SHOPLIFTING", "BURGLARY",
-              "PROPERTY DAMAGE", "PROSTITUTION", "ROBBERY",
-              "AUTO THEFTS", "THREATS, HARASSMENT", "TRESPASS"
-            ))
-          )
-        ),
-        mainPanel(
-          tabsetPanel(
-            type = "tabs",
-            tabPanel(
-              "Plot",
-              h1("Crime Frequency Map"),
-              textOutput("plot_description"),
-              plotOutput("map", click = "plot_click"),
-              verbatimTextOutput("info"),
-              p(strong("District:"), textOutput("district_point", inline = TRUE)),
-              p(strong("Frequency:"), textOutput("frequency_district", inline = TRUE)),
-              br(),
-              textOutput("plot_interactive"),
-              leafletOutput("interactive_map"),
-              p(strong("5 Districts With The Most Crimes:"), tableOutput("most_dangerous")),
-              p(strong("5 Districts With The Least Crimes:"), tableOutput("most_safe"))
-            )
-          )
-        )
-      )
+    tabPanel("Areas of Crime",
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput('year',
+                             "Choose a year:",
+                             min = 2014,
+                             max = 2018,
+                             value = 2016,
+                             sep = ""),
+                 selectInput('crime_type',
+                             "Select a crime",
+                             choices = list('Crimes' = c("ASSAULTS", "BURGLARY",
+                                                         "PROPERTY DAMAGE", "PROSTITUTION", "ROBBERY",
+                                                         "NARCOTICS COMPLAINTS", "TRESPASS", "HOMICIDE")))
+               ),
+               mainPanel(
+                 h1("Crime Frequency Maps"),
+                 textOutput("plot_description"),
+                 plotOutput('map', click = "plot_click"),
+                 verbatimTextOutput("info"),
+                 p(strong("District:"), textOutput("district_point", inline = TRUE)),
+                 p(strong("Frequency:"), textOutput("frequency_district", inline = TRUE)),
+                 br(),
+                 textOutput("plot_interactive"),
+                 leafletOutput('interactive_map'),
+                 p(strong("5 Districts With The Most Crimes:"), tableOutput("most_dangerous")),
+                 p(strong("5 Districts With The Least Crimes:"), tableOutput("most_safe")),
+                 br(),
+                 h1("Selected Crime/Year Analysis"),
+                 textOutput("selected_analysis"),
+                 br(),
+                 h1("Overall Analysis"),
+                 textOutput("overall_analysis"),
+                 br()
+               )
+             )
     ),
     tabPanel("Car Accidents",
       sidebarLayout(
